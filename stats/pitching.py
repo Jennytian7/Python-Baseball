1ad80660de160dea9acd52f8ef485b1bdddec7cd
JennyTian7 <37425050+Jennytian7@users.noreply.github.com>
2020-06-12 19:41:57 -0400

    Update pitching.py

--- stats/pitching.py
+++ stats/pitching.py
@@ -2,13 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from data import games
-plays =  games.loc[(games['type'] == 'play'), :]
+plays =  games['type'][games['type'] == 'play']
+# games.loc[(games['type'] == 'play'), :]
 strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 strike_outs = strike_outs.reset_index(name='strike_outs')#grouby to dataframe
 #5Apply an Operation to Multiple Columns
 strike_outs = strike_outs.loc[:, ['year',  'strike_outs']].apply(pd.to_numeric)
 # strike_outs.head()
-strike_outs.plot.scatter(x = 'year', y='strike_outs')
+strike_outs.plot(x = 'year', y='strike_outs', kind ='scatter')
 plt.legend('Strike_outs')
 plt.show()
